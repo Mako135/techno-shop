be887df45df1124cb5e47d68edf537be8a50a539
Kuanyshbek.M <Kuanyshbek.M@kazteleport.kz>
2024-05-13 12:52:05 +0500

    addind css

--- frontend/src/pages/AnimatedPhonePages/pova/components/ColorSection.jsx
+++ frontend/src/pages/AnimatedPhonePages/pova/components/ColorSection.jsx
@@ -40,7 +40,7 @@ const ColorSection = () => {
 
   return (
     <div style={{ position: "relative" }}>
-      <div className="slogan">
+      <div className="slider-slogan">
         <h1>Здесь ваш слоган!</h1>
       </div>
 
